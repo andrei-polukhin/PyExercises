f8ba4e27dd39597b4acdc35cc0f203a8d6ebef89
AndrewPolukhin <qq7039108@gmail.com>
2020-08-10 21:11:23 +0300

    Added an async request exercise

--- AsynchronousDevelopment/async-request.py
+++ AsynchronousDevelopment/async-request.py
@@ -1,13 +1,30 @@
-import aiohttp
 import asyncio
+import aiohttp
 
 
-async def fetch_page(url):
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
-            return response.status
+async def fetch_page(session, url):
+    async with session.get(url) as response:
+        return response.status
 
 
-loop = asyncio.get_event_loop()
-tasks = [fetch_page("https://google.com") for i in range(1000)]
-print(loop.run_until_complete(asyncio.gather(*tasks)))
+async def get_multiple_pages(loop, *urls):
+    tasks = []
+    async with aiohttp.ClientSession(loop=loop) as session:
+        for url in urls:
+            tasks.append(fetch_page(session, url))
+        return await asyncio.gather(*tasks)
+
+
+if __name__ == '__main__':
+    def main():
+        loop = asyncio.get_event_loop()
+        urls = [
+            'http://google.com',
+            'http://example.com',
+            'http://tecladocode.com/blog'
+        ]
+        pages = loop.run_until_complete(get_multiple_pages(loop, *urls))
+        for page in pages:
+            print(page)
+        loop.close()
+    print(main())
