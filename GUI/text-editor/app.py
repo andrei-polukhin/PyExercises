6d1e1c270496df9bcdf2d3dfe185ba5940fbf4bb
AndrewPolukhin <qq7039108@gmail.com>
2020-09-08 11:07:37 +0300

    Added a menu to the text editor project

--- GUI/text-editor/app.py
+++ GUI/text-editor/app.py
@@ -11,14 +11,22 @@ def create_file():
 
 root = tk.Tk()
 root.title("Text-Editor-Redefined")
+root.option_add("*tearOff", False)
 
 main = ttk.Frame(root)
 main.pack(fill="both", expand=True, padx=1, pady=(5,0))
 
+menubar = tk.Menu()
+root.config(menu=menubar)
+
+file_menu = tk.Menu(menubar)
+menubar.add_cascade(menu=file_menu, label="File")
+
+file_menu.add_command(label="New", command=create_file)
+
 notebook = ttk.Notebook(main)
 notebook.pack(fill="both", expand=True)
 
-create_file()
 create_file()
 
 root.mainloop()
