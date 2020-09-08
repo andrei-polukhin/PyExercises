5b9728328e96bb2f492ee90a98da51b8965ce469
AndrewPolukhin <qq7039108@gmail.com>
2020-09-08 11:30:56 +0300

    Added saving the file

--- GUI/text-editor/app.py
+++ GUI/text-editor/app.py
@@ -1,5 +1,6 @@
+import os
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk, filedialog
 
 
 def create_file():
@@ -7,6 +8,20 @@ def create_file():
     text_area.pack(fill="both", expand=True)
     notebook.add(text_area, text="Untitled")
     notebook.select(text_area)
+
+
+def save_file():
+    filepath = filedialog.asksaveasfilename()
+    try:
+        filename = os.path.basename(filepath)
+        text_widget = root.nametowidget(notebook.select())
+        content = text_widget.get("1.0", "end-1c")
+        with open(filepath, "w") as file:
+            file.write(content)
+    except (AttributeError, FileNotFoundError):
+        print("Save operation cancelled")
+        return
+    notebook.tab("current", text=filename)
 
 
 root = tk.Tk()
@@ -23,6 +38,7 @@ file_menu = tk.Menu(menubar)
 menubar.add_cascade(menu=file_menu, label="File")
 
 file_menu.add_command(label="New", command=create_file)
+file_menu.add_command(label="Save", command=save_file)
 
 notebook = ttk.Notebook(main)
 notebook.pack(fill="both", expand=True)
