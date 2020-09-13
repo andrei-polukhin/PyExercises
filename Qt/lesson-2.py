f4945b8c9cd529f557374136a7e2f76568eb75d8
AndrewPolukhin <qq7039108@gmail.com>
2020-09-13 16:22:21 +0300

    Added the second sentdex exercise

---
+++ Qt/lesson-2.py
@@ -0,0 +1,17 @@
+import sys
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
+
+
+class Window(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(50, 50, 500, 300)
+        self.setWindowTitle("My OOP PyQt programme")
+        self.setWindowIcon(QIcon("python.png"))
+        self.show()
+
+
+app = QApplication(sys.argv)
+GUI = Window()
+sys.exit(app.exec_())
