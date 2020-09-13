c5e79fb8937b2408c55c75c8f6641f3a73224586
AndrewPolukhin <qq7039108@gmail.com>
2020-09-13 17:14:05 +0300

    Added a label to my programme

--- Qt/lesson_2.py
+++ Qt/lesson_2.py
@@ -1,5 +1,6 @@
 import sys
 from PyQt5.QtGui import QIcon
+from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
 
@@ -9,12 +10,19 @@ class Window(QMainWindow):
         self.setGeometry(200, 200, 500, 300)
         self.setWindowTitle("My OOP PyQt programme")
         self.setWindowIcon(QIcon("python.png"))
-        self.show()
+
+    def add_label(self):
+        label = QtWidgets.QLabel(self)
+        label.setText("Hi, this is my application")
+        label.adjustSize()
+        label.move(50, 50)
 
 
 def run():
     app = QApplication(sys.argv)
     gui = Window()
+    gui.add_label()
+    gui.show()
     sys.exit(app.exec_())
 
 
