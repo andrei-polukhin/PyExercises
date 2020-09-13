8312bfc9970e3c2fdb0acc5fe9445a051e04f8a1
AndrewPolukhin <qq7039108@gmail.com>
2020-09-13 17:03:31 +0300

    Added a bit more generic styling for functions in PyQt5

--- Qt/lesson_2.py
+++ Qt/lesson_2.py
@@ -6,12 +6,16 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 class Window(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setGeometry(50, 50, 500, 300)
+        self.setGeometry(200, 200, 500, 300)
         self.setWindowTitle("My OOP PyQt programme")
         self.setWindowIcon(QIcon("python.png"))
         self.show()
 
 
-app = QApplication(sys.argv)
-GUI = Window()
-sys.exit(app.exec_())
+def run():
+    app = QApplication(sys.argv)
+    gui = Window()
+    sys.exit(app.exec_())
+
+
+run()
