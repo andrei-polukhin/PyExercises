7b3f94c7a9c94997b074832095e120b32f5742fb
AndrewPolukhin <qq7039108@gmail.com>
2020-09-25 15:35:28 +0300

    Added two exercises on asynchronous development

---
+++ AsynchronousDevelopment/maths_async.py
@@ -0,0 +1,31 @@
+import asyncio
+import time
+
+
+async def calculate_number(number):
+    answer = [x**2 for x in range(1, number+1)]
+    return answer
+
+
+async def get_numbers(*numbers):
+    tasks = []
+    for number in numbers:
+        tasks.append(calculate_number(number))
+    return await asyncio.gather(*tasks)
+
+
+def main():
+    before = time.time()
+    loop = asyncio.get_event_loop()
+    numbers = [
+      1, 3, 6, 10, 16, 21, 420, 500, 3200, 12900, 23001
+    ]
+    results = loop.run_until_complete(get_numbers(*numbers))
+    for result in results:
+        print(result)
+    loop.close()
+    print(f"The time spent on maths with async is {time.time()-before}s")
+
+
+if __name__ == "__main__":
+    main()
---
+++ AsynchronousDevelopment/maths_no_async.py
@@ -0,0 +1,18 @@
+import time
+
+
+def calculate_number(number):
+    answer = [x**2 for x in range(1, number+1)]
+    return answer
+
+
+def main():
+    before = time.time()
+    numbers = [1, 3, 6, 10, 16, 21, 420, 500, 3200, 12900, 23001]
+    for number in numbers:
+        print(calculate_number(number))
+    print(f"The time spent for maths without async is {time.time()-before}s")
+
+
+if __name__ == "__main__":
+    main()
