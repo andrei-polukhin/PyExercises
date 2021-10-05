3df0fc5f5b583c8fefea4b3de9dd951ab218709c
Andrew Polukhin <qq7039108@gmail.com>
2021-10-05 08:17:35 +0300

    Restructuring

---
+++ SQL/advanced/stored_procedures/02_variables.sql
@@ -0,0 +1,36 @@
+/* #2: working with variables, single-row return */
+CREATE OR REPLACE FUNCTION tr_clients_balances_events()
+ RETURNS trigger
+ LANGUAGE plpgsql
+AS $$
+DECLARE
+  _events_id varchar;
+  _events_value jsonb;
+BEGIN
+  _events_id = CASE
+    WHEN NEW.balance <= 0 THEN 'clients.balance_zero'
+    ELSE 'clients.balance_notzero'
+    END;
+  
+  _events_value = jsonb_build_object(
+        'dt', NOW(),
+        'object_id', NEW.clients_id::varchar,
+        'events_id', _events_id
+      );
+
+  INSERT INTO
+    ex_events_queue (ex_events_id, data, object_id)
+    VALUES
+
+      (events_id,
+
+      jsonb_build_object(
+      'event', _events_value,
+      'data', '{}'::jsonb
+      ),
+
+      NEW.clients_id::varchar);
+
+  RETURN NEW;
+END;
+$$;
