5180a06ae3f88016d90a056cfb04db40dcf109fb
Andrew Polukhin <qq7039108@gmail.com>
2021-10-12 21:56:42 +0300

    3 examples - different types of returns

--- SQL/advanced/stored_procedures/02_variables.sql
+++ SQL/advanced/stored_procedures/02_variables.sql
@@ -1,36 +1,53 @@
-/* #2: working with variables, single-row return */
-CREATE OR REPLACE FUNCTION tr_clients_balances_events()
- RETURNS trigger
- LANGUAGE plpgsql
+/* #2: variables return */
+
+-- RETURNS <primitive_type>
+CREATE OR REPLACE FUNCTION get_exchange_rate(_from_currency varchar, _to_currency varchar, _dt timestamp without time zone)
+    RETURNS numeric
+    LANGUAGE plpgsql
 AS $$
-DECLARE
-  _events_id varchar;
-  _events_value jsonb;
-BEGIN
-  _events_id = CASE
-    WHEN NEW.balance <= 0 THEN 'clients.balance_zero'
-    ELSE 'clients.balance_notzero'
-    END;
-  
-  _events_value = jsonb_build_object(
-        'dt', NOW(),
-        'object_id', NEW.clients_id::varchar,
-        'events_id', _events_id
-      );
+    DECLARE
+        _exchange_rate numeric;
 
-  INSERT INTO
-    ex_events_queue (ex_events_id, data, object_id)
-    VALUES
+    -- this is pretty abstract, but I remember we have currencies as a table in our billing system, so it should be like this (?!)
+    SELECT
+        exchange_rate
+    INTO
+        _exchange_rate
+    FROM
+        currencies
+    WHERE
+        from_name = _from_currency
+        AND to_name = _to_currency
+        AND dt = _dt;
 
-      (_events_id,
+    RETURN _exchange_rate;
+$$;
 
-      jsonb_build_object(
-      'event', _events_value,
-      'data', '{}'::jsonb
-      ),
+-- RETURNS %rowtype
+CREATE OR REPLACE FUNCTION get_code_info(_code varchar)
+    RETURNS codes%rowtype
+    LANGUAGE SQL
+AS $$
+    SELECT
+        *
+    FROM
+        codes  -- again abstract
+    WHERE
+        code = _code;
+$$;
 
-      NEW.clients_id::varchar);
-
-  RETURN NEW;
-END;
+-- OUT...
+CREATE OR REPLACE FUNCTION get_rate_main_info(
+    IN _code varchar, IN _rate_tables_id varchar,
+    OUT rate_per_min numeric, OUT pay_setup integer, OUT grace_volume integer, OUT min_interval integer
+)
+LANGUAGE SQL
+AS $$
+    SELECT
+        rate_per_min, pay_setup, grace_volume, min_interval
+    FROM
+        rates
+    WHERE
+        rate_tables_id = _rate_tables_id
+        AND code = _code;
 $$;
