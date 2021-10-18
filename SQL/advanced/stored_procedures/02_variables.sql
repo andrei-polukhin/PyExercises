77835a429ee2d13589ff691c90f016a1a80c8506
Andrew Polukhin <qq7039108@gmail.com>
2021-10-18 21:24:08 +0300

    02 - variables: sql -> plpgsql and logical fixes

--- SQL/advanced/stored_procedures/02_variables.sql
+++ SQL/advanced/stored_procedures/02_variables.sql
@@ -9,17 +9,16 @@ AS $$
         _exchange_rate numeric;
     BEGIN
 
-    -- this is pretty abstract, but I remember we have currencies as a table in our billing system, so it should be like this (?!)
-    SELECT
-        exchange_rate
-    INTO
-        _exchange_rate
-    FROM
-        currencies
-    WHERE
-        from_name = _from_currency
-        AND to_name = _to_currency
-        AND dt = _dt;
+    _exchange_rate := exchange_rate
+        FROM
+            currencies_table
+        WHERE
+            from_currency = _from_currency
+            AND to_currency = _to_currency
+            AND dt <= _dt
+        ORDER BY
+            dt DESC
+        LIMIT 1;
 
     RETURN _exchange_rate;
     END;
@@ -27,24 +26,39 @@ $$;
 
 -- RETURNS %rowtype
 CREATE OR REPLACE FUNCTION get_code_info(_code varchar)
-    RETURNS codes%rowtype
-    LANGUAGE SQL
+    RETURNS TABLE codes  -- more than several codes meet this requirement :)
+    LANGUAGE plpgsql
 AS $$
+    BEGIN
+
+    RETURN QUERY
     SELECT
         *
     FROM
         codes  -- again abstract
     WHERE
         code = _code;
+
+    RETURN _selected_code;
+    END;
 $$;
 
 -- OUT...
+CREATE TYPE main_rate_info AS (
+    rate_per_min numeric,
+    pay_setup integer,
+    grace_volume integer,
+    min_interval integer
+);
+
 CREATE OR REPLACE FUNCTION get_rate_main_info(
     IN _code varchar, IN _rate_tables_id varchar,
     OUT rate_per_min numeric, OUT pay_setup integer, OUT grace_volume integer, OUT min_interval integer
 )
-LANGUAGE SQL
+LANGUAGE plpgsql
+RETURNS main_rate_info
 AS $$
+    RETURN QUERY
     SELECT
         rate_per_min, pay_setup, grace_volume, min_interval
     FROM
