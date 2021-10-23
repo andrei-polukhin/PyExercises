5431f31229dccdc9da9b2e93cbc21e8bd9292476
Andrew Polukhin <qq7039108@gmail.com>
2021-10-23 09:36:59 +0300

    Single-row return: after-discussion first variant

--- SQL/advanced/stored_procedures/02_variables.sql
+++ SQL/advanced/stored_procedures/02_variables.sql
@@ -1,7 +1,7 @@
 /* #2: variables return */
 
 -- RETURNS <primitive_type>
-CREATE OR REPLACE FUNCTION get_exchange_rate(_from_currency varchar, _to_currency varchar, _dt timestamp without time zone)
+CREATE OR REPLACE FUNCTION get_exchange_rate(_from_currency varchar, _to_currency varchar, _dt timestamp with time zone)
     RETURNS numeric
     LANGUAGE plpgsql
 AS $$
@@ -26,32 +26,41 @@ $$;
 
 -- RETURNS %rowtype
 CREATE OR REPLACE FUNCTION get_code_info(_code varchar)
-    RETURNS codes%rowtype
+    RETURNS record
     LANGUAGE plpgsql
 AS $$
+    DECLARE
+        _ret record;
+
     BEGIN
 
-    RETURN QUERY
     SELECT
         *
+    INTO
+        _ret
     FROM
-        codes  -- again abstract
+        codes
     WHERE
         code = _code
     LIMIT 1;
 
+    IF NOT FOUND THEN
+        RAISE EXCEPTION 'No such code';
+    END IF;
+
+    RETURN _ret;
     END;
 $$;
 
 -- OUT...
 CREATE OR REPLACE FUNCTION get_rate_main_info(
-    IN _code varchar, IN _rate_tables_id varchar,
+    IN _code varchar, IN _rate_tables_id integer,
     OUT rate_per_min numeric, OUT pay_setup integer, OUT grace_volume integer, OUT min_interval integer
 )
 LANGUAGE plpgsql
 AS $$
-    RETURN QUERY
-    SELECT
+    BEGIN
+    SELECT INTO
         rate_per_min, pay_setup, grace_volume, min_interval
     FROM
         rates
@@ -59,4 +68,10 @@ AS $$
         rate_tables_id = _rate_tables_id
         AND code = _code
     LIMIT 1;
+
+    IF NOT FOUND THEN
+        RETURN;
+    END IF;
+
+    END;
 $$;
