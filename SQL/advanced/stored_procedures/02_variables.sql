065dffb2e77b1d86e922e0f255b2ea6531f72396
Andrew Polukhin <qq7039108@gmail.com>
2021-10-26 19:41:01 +0300

    Formatting + EXECUTE

--- SQL/advanced/stored_procedures/02_variables.sql
+++ SQL/advanced/stored_procedures/02_variables.sql
@@ -26,69 +26,65 @@ $$;
 
 -- OUT...
 CREATE OR REPLACE FUNCTION get_code_info(
-    IN _code varchar,
-    IN _code_decks_id integer,
-    OUT name varchar, OUT country varchar
+  IN _code varchar,
+  IN _code_decks_id integer,
+  OUT name varchar, OUT country varchar
 )
-    LANGUAGE plpgsql
+  LANGUAGE plpgsql
 AS $$
-    DECLARE
-        _code_row record;
+DECLARE
+  _code_row record;
+BEGIN
+  SELECT
+    *
+  INTO STRICT
+    _code_row
+  FROM
+    codes
+  WHERE
+    code = _code
+    AND code_decks_id = _code_decks_id;
 
-    BEGIN
+  EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+      RAISE NOTICE 'No codes found!';
+      RETURN;
+    WHEN TOO_MANY_ROWS THEN
+      RAISE NOTICE 'Several codes satisfy specified parameters!';
+      RETURN;
 
-    SELECT
-        *
-    INTO STRICT
-        _code_row
-    FROM
-        codes
-    WHERE
-        code = _code
-        AND code_decks_id = _code_decks_id;
+  name := _code_row.name;
+  country := _code_row.country;
 
-    EXCEPTION
-        WHEN no_data_found THEN
-            RAISE NOTICE 'No codes found!';
-            RETURN;
-        WHEN too_many_rows THEN
-            RAISE NOTICE 'Several codes satisfy specified parameters!';
-            RETURN;
-
-    name := _code_row.name;
-    country := _code_row.country;
-
-    END;
+END;
 $$;
 
 -- SETOF...
 CREATE OR REPLACE FUNCTION get_rate_main_info(
-    IN _code varchar, IN _rate_tables_id integer,
-    OUT rate_per_min numeric, OUT pay_setup integer, OUT grace_volume integer, OUT pay_interval integer
+  IN _code varchar, IN _rate_tables_id integer,
+  OUT rate_per_min numeric, OUT pay_setup integer, OUT grace_volume integer, OUT pay_interval integer
 ) RETURNS SETOF record
 LANGUAGE plpgsql
 AS $$
-    DECLARE
-        rec record;
+DECLARE
+  rec record;
+  query text := '';
+BEGIN
+  query := 'SELECT
+      *
+    FROM
+      rates
+    WHERE
+      rate_tables_id = _rate_tables_id
+      AND code = _code
+    ORDER BY id';
 
-    BEGIN
-
-    FOR rec IN (
-        SELECT
-            *
-        FROM
-            rates
-        WHERE
-            rate_tables_id = _rate_tables_id
-            AND code = _code
-        ORDER BY id
-    ) LOOP
-        rate_per_min := rec.rate_per_min;
-        pay_setup := rec.pay_setup;
-        grace_volume := rec.grace_volume;
-        pay_interval := rec.pay_interval;
-        RETURN NEXT;
-    END LOOP;
-
-    END;
+  FOR rec IN EXECUTE query LOOP
+    rate_per_min := rec.rate_per_min;
+    pay_setup := rec.pay_setup;
+    grace_volume := rec.grace_volume;
+    pay_interval := rec.pay_interval;
+    RETURN NEXT;
+  END LOOP;
+END;
 $$;
