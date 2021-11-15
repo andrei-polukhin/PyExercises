8c15416b55cc32762580c1beaac349173aafc6c6
Andrew Polukhin <qq7039108@gmail.com>
2021-11-15 14:49:13 +0200

    Minor logical changes

--- CodeDesign/leaves.py
+++ CodeDesign/leaves.py
@@ -4,7 +4,7 @@ Let employees apply to leaves: medical, unpaid, paid, etc.
 
 from datetime import datetime
 
-import tinyrpcxt
+from tinyrpc import dispatcher
 from formencode import validators
 
 from .pgsql import pgsql
@@ -14,13 +14,13 @@ class Schema:
     """Schema for the ``apply_to_leave`` API method"""
 
     employees_id = validators.Int(not_empty=True)
-    leaves_type = validators.OneOf(['medical', 'paid', 'unpaid'])
+    leaves_type = validators.OneOf(['medical', 'paid', 'unpaid'], not_empty=True)
     start_date = validators.DateValidator(not_empty=True)
     end_date = validators.DateValidator(not_empty=True)
     notes = validators.String()
 
 
-@tinyrpcxt.public(schema=Schema)
+@dispatcher.public(schema=Schema)
 def apply_to_leave(
   employees_id: int, leaves_type: str, start_date: datetime, end_date: datetime, notes: str
 ) -> None:
@@ -31,10 +31,10 @@ def apply_to_leave(
         raise Exception('No such an employee!')
 
     data = {
-    'employees_id': employees_id,
-    'type': leaves_type,
-    'start_date': start_date,
-    'end_date': end_date,
-    'notes': notes,
+        'employees_id': employees_id,
+        'type': leaves_type,
+        'start_date': start_date,
+        'end_date': end_date,
+        'notes': notes,
     }
     return pgsql().query.insert('leaves', data)
